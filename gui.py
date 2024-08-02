832b43159b5df20e05159e344a99c4b4d30dbcf4
Eduardo Monteiro de Sousa <edu123monteiro@gmail.com>
2024-08-02 15:40:31 -0300

    GUI update

--- gui.py
+++ gui.py
@@ -5,60 +5,98 @@ import numpy as np
 import cv2
 import time
 import colorama
+import json
+import vosk
+import speech_recognition as sr
+import pyttsx3
+from colorama import Fore, Back, Style
 colorama.init()
+from control.http import client
+from comandos import command_dict
 
 # Configurações de gravação
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 16000
-CHUNK = 2048
+FRAME_RATE = 16000
+CHUNK = 8000
+
+# Inicializa o recognizer e o motor de texto para fala
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+     engine.say(text)
+     engine.runAndWait()
+
 
 # Inicializa PyAudio
 p = pyaudio.PyAudio()
-stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-
-# URL do stream da câmera IP
-camera_url = 'https://www.earthcam.com/usa/newyork/timessquare/?cam=tsrobo1'
-cap = cv2.VideoCapture(camera_url)
+stream = p.open(format=FORMAT, channels=CHANNELS, rate=FRAME_RATE, input=True, frames_per_buffer=CHUNK)
+stream.start_stream()
 
 # Título do aplicativo
 st.title("Captura de Áudio")
 
 aux_area = st.empty()
+if 'model' not in st.session_state:
+    # Configure o modelo Vosk (baixe um modelo adequado para seu idioma)
+    new_model = vosk.Model("models/vosk-model-small-pt-0.3")
+    rec = vosk.KaldiRecognizer(new_model, FRAME_RATE)
+    st.session_state.model = new_model
+    st.session_state.rec = rec 
+
+text_area = st.empty()
 
 
+# # Inicia a captura de áudio
+# audio_frames = []
+# ret, frame = cap.read()
+# try:
+#     if not ret:
+#         aux_area.write("Erro ao capturar vídeo.")
 
-# Inicia a captura de áudio
-audio_frames = []
-status_placeholder = st.empty()
+#     # Converte a imagem de BGR para RGB
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+#     # Exibe o frame no Streamlit
+#     st.image(frame, channels='RGB', use_column_width=True)
+# except:
+#     pass
+
 while True:
-    ret, frame = cap.read()
-    try:
-        if not ret:
-            aux_area.write("Erro ao capturar vídeo.")
-
-        # Converte a imagem de BGR para RGB
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Exibe o frame no Streamlit
-        st.image(frame, channels='RGB', use_column_width=True)
-    except:
-        pass
-
-
     try:
         data = stream.read(CHUNK)
-        status_placeholder.write(data)
-        audio_frames.append(data)
+        if rec.AcceptWaveform(data):
+            result = json.loads(rec.Result())
+            print(f"Você disse: {result}")
+            text_area.write(result['text'])
+            ## Comandos
+            if "esquerda" in result["text"]:
+                print(Fore.GREEN + f'SENDING COMMAND "{result["text"]}" TO ESP8266: ' + Style.RESET_ALL)
+                client.send_command(command_dict['esquerda'])
+            elif "direita" in result["text"]:
+                print(Fore.GREEN + f'SENDING COMMAND "{result["text"]}" TO ESP8266: ' + Style.RESET_ALL)
+                client.send_command(command_dict['direita'])
+            elif "baixo" in result["text"]:
+                print(Fore.GREEN + f'SENDING COMMAND "{result["text"]}" TO ESP8266: ' + Style.RESET_ALL)
+                client.send_command(command_dict['baixo'])
+            elif "cima" in result["text"]:
+                print(Fore.GREEN + f'SENDING COMMAND "{result["text"]}" TO ESP8266: ' + Style.RESET_ALL)
+                client.send_command(command_dict['cima'])
+            elif "abrir garra" in result["text"]:
+                print(Fore.GREEN + f'SENDING COMMAND "{result["text"]}" TO ESP8266: ' + Style.RESET_ALL)
+                client.send_command(command_dict['abrir garra'])
+            elif "fechar garra" in result["text"]:
+                print(Fore.GREEN + f'SENDING COMMAND "{result["text"]}" TO ESP8266: ' + Style.RESET_ALL)
+                client.send_command(command_dict['fechar garra'])
+            elif "escovação" in result["text"]:
+                print(Fore.GREEN + f'SENDING COMMAND "{result["text"]}" TO ESP8266: ' + Style.RESET_ALL)
+                client.send_command(command_dict['escovação'])
     except:
-        break
-
-# Para o stream de áudio
-stream.stop_stream()
-stream.close()
-p.terminate()
-# Libera o objeto de captura quando terminar
-cap.release()
+        # Para o stream de áudio
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
 
 # # Salva o áudio gravado
 # if audio_frames:
